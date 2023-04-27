262ec3c5617817df25864d24aefe465b48ae4aac
Ariana Alvarez <57126724+arianaalvarezv@users.noreply.github.com>
2023-04-27 18:00:27 +0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+# import altair as alt
+# import math
+# import pandas as pd
+import streamlit as st
+
+
+#confit
+st.set_page_config(
+    page_title = "InspireAI", 
+    page_icon="💡",
+)
+st.markdown("<h1 style='text-align: center;'>InspireAI - Your personal tutor ChatBot 💡</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Choose the expertise you are looking for to maximize your learning.</h1>", unsafe_allow_html=True)
+
+
+#Title
+# st.title('InspireAI')
+
+#Side Bar
+st.sidebar.success("Select an AI Tutor")
+
+#Content
+# st.write(
+#     """
+#     Choose the expertise you are looking for.
+#     """
+# )
+
+
+
+
+
---
+++ pages/3_📈_Finance.py
@@ -0,0 +1,16 @@
+import openai
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+from streamlit_chat import message
+
+# Config
+st.set_page_config(page_title='Economics - InspireAI', page_icon='💡', layout='wide')
+# st.markdown("<h1 style='text-align: center;'>InspireAI - Your personal tutor ChatBot 💡</h1>", unsafe_allow_html=True)
+
+# Set API key
+openai.api_key = "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"
+
+# Title
+st.title('📈 Finance AI Tutor')
