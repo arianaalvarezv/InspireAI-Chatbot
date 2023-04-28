7d16ce73a442e80413148d3883a3478f987de54f
Ariana Alvarez <57126724+arianaalvarezv@users.noreply.github.com>
2023-04-28 13:40:11 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,8 @@ from collections import namedtuple
 # import math
 # import pandas as pd
 import streamlit as st
-
+# import streamlit_extras
+# from streamlit_extras.switch_page_button import switch_page
 
 #confit
 st.set_page_config(
@@ -27,6 +28,23 @@ st.sidebar.success("Select an AI Tutor")
 #     """
 # )
 
+c1, c2, c3, c4 = st.columns(4)
+with c1:
+    # st.info("**Writing AI Tutor <a href='Writing' target='1_📝_Writing'>Writing AI Tutor</a>**", icon="📝")
+    # writing = st.button("Writing AI Tutor")
+    st.info('**[Writing AI Tutor](http://localhost:8501/Writing)**')
+    # st.markdown("<a href='Writing' target='1_📝_Writing'>Writing AI Tutor</a>", unsafe_allow_html=True)
+
+with c2:
+    # st.markdown("<a href='Economics' target='2_💰_Economics'>Economics AI Tutor</a>", unsafe_allow_html=True)
+    st.info('**[Economics AI Tutor](http://localhost:8501/Economics)**')
+with c3:
+    # st.markdown("<a href='Finance' target='3_📈_Finance'>Finance AI Tutor</a>", unsafe_allow_html=True)
+    st.info('**[Finance AI Tutor](http://localhost:8501/Finance)**')
+
+with c4:
+    # st.markdown("<a href='Computer_Science' target='4_💻_Computer_Science'>Computer Science AI Tutor</a>", unsafe_allow_html=True)
+    st.info('**[Computer Science AI Tutor](http://localhost:8501/Computer_Science)**')
 
 
 
